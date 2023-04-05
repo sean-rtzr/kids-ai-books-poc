bfa1bf2dcdf963f7f0b79146d86b4e16ad10e626
sean-rtzr <sean@rtzr.ai>
2023-04-05 16:51:09 +0900

    홈화면 시작.

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    우리AI북스
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,13 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify({
+    components,
+    directives,
+});
